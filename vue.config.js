ce1974f733239794d37fa668bf45a9ce165b19f3
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2021-10-02 09:26:41 -0300

    chore: add port variable in .env

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,9 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? process.env.VUE_APP_CURRENT_SITE_ADDRESS
     : '/',
+  devServer: {
+    port: process.env.VUE_APP_PORT || '8080',
+  },
   lintOnSave: false,
   chainWebpack: (config) => {
     config.resolve.alias
