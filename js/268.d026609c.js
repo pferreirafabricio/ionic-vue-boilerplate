b8cc71c0d2fe2d5270f4f044810631e904aed3ee
pferreirafabricio <pferreirafabricio@gmail.com>
2023-10-09 23:18:21 +0000

    Deploying to gh-pages from @ pferreirafabricio/ionic-vue-boilerplate@fe369b34ab40565160ca979de6e727dafcdb4559 🚀

--- js/268.d026609c.js
+++ js/268.d026609c.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkapp_boilerplate"]=self["webpackChunkapp_boilerplate"]||[]).push([[268],{2268:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var u=t(6252);function a(e,n){const t=(0,u.up)("base-layout");return(0,u.wg)(),(0,u.j4)(t,null,{default:(0,u.w5)((()=>[(0,u.Uk)(" Home client ")])),_:1})}var r=t(3744);const l={},p=(0,r.Z)(l,[["render",a]]);var c=p}}]);
-//# sourceMappingURL=268.c2455042.js.map
+//# sourceMappingURL=268.d026609c.js.map
