002e5d4addc91825ebfeed962df5b643685f4069
LeoNardoRR <ribeiroleonardoti@gmail.com>
2023-11-07 19:51:14 -0300

    remove code button delete :(

--- src/pages/home/examples/Buttons.vue
+++ src/pages/home/examples/Buttons.vue
@@ -11,13 +11,6 @@
 />
 
 <Button
-  class="delete"
-  text = "Delete" :icon = "Icons.save"
-  color = "danger"
-/>
-<hr/>
-
-<Button
   is-loading
   text="Loading" :icon="Icons.save"
   color="success"
@@ -79,56 +72,6 @@ const Icons = ref({
 .icon:hover {
   transition: transform 0.3s ease, border-radius 0.3s ease;
   transform: scale(1.02);
-}
-
-.delete {
-  border-radius: 5px;
-  box-shadow: 1px 1px 3px rgba(0,0,0,0.15);
-}
-
-.delete, button span {
-  transition: 200ms;
-}
-
-.delete .text {
-  transform: translateX(35px);
-  color: white;
-  font-weight: bold;
-}
-
-.delete .icon {
-  position: relative;
-  border-left: 1px solid #c41b1b;
-  transform: translateX(110px);
-  height: 40px;
-  width: 40px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.delete svg {
-  width: 15px;
-  margin-left: 60px;
-  fill: #eee;
-}
-
-.delete:hover .text {
-  color: transparent;
-}
-
-.delete:hover .icon {
-  width: 150px;
-  border-left: none;
-  transform: translateX(0);
-}
-
-.delete:focus {
-  outline: none;
-}
-
-.delete:active .icon svg {
-  transform: scale(0.8);
 }
 
 </style>
